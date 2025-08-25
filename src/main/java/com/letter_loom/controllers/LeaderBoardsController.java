8a4d9e9a3b81c42705c9dcc04f64d2ef4e2fc49d
Ivan Paguyo <ezrapaguyo09@gmail.com>
2025-08-25 23:04:42 +0800

    minor updates for leaderboards

--- src/main/java/com/letter_loom/controllers/LeaderBoardsController.java
+++ src/main/java/com/letter_loom/controllers/LeaderBoardsController.java
@@ -5,6 +5,7 @@ import com.letter_loom.dtos.response_dto.LeaderboardScoresResponse;
 import com.letter_loom.dtos.response_dto.LeaderboardWinsResponse;
 import com.letter_loom.services.LeaderboardService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,14 +19,14 @@ public class LeaderBoardsController {
 
     private final LeaderboardService leaderboardServices;
 
-    @GetMapping("win")
-    public List<LeaderboardWinsResponse> getLeaderBoards(){
-        return leaderboardServices.getLeaderboardWinInfo();
+    @GetMapping("/win")
+    public ResponseEntity<List<LeaderboardWinsResponse>> getLeaderBoards(){
+        return ResponseEntity.ok(leaderboardServices.getLeaderboardWinInfo());
     }
 
 
-    @GetMapping("score")
-    public List<LeaderboardScoresResponse> getLeaderboardScores(){
-        return leaderboardServices.getLeaderboardScoreInfo();
+    @GetMapping("/score")
+    public ResponseEntity<List<LeaderboardScoresResponse>> getLeaderboardScores(){
+        return ResponseEntity.ok(leaderboardServices.getLeaderboardScoreInfo());
     }
 }
--- src/main/java/com/letter_loom/controllers/LoginLogoutController.java
+++
@@ -1,9 +0,0 @@
-package com.letter_loom.controllers;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class LoginLogoutController {
-    public void login(){}
-    public void logout(){}
-}
