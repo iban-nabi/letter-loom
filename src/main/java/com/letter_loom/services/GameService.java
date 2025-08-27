e17c1fb3c07d10b70f96f52535de2d30c45309bd
Ivan Paguyo <ezrapaguyo09@gmail.com>
2025-08-27 23:59:39 +0800

    Implement Letter Generator and Word Validator

--- src/main/java/com/letter_loom/services/GameService.java
+++ src/main/java/com/letter_loom/services/GameService.java
@@ -1,11 +1,54 @@
 package com.letter_loom.services;
 
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 @Service
+@RequiredArgsConstructor
 public class GameService {
-    public void generateLetters(){}
-    public void verifyWord(){}
+    //for letter generator
+    private static final String vowels = "AEIOU";
+    private static final String consonants = "BCDFGHJKLMNPQRSTVWXYZ";
+    private static final int consonantLength = 6;
+    private static final int vowelsLength = 4;
+    private static final Random random = new Random();
+
+    @Value("${dictionary.apiUrl}")
+    private String dictionaryApiUrl;
+
+    public List<Character> generateLetters(){
+        List<Character> letters = new ArrayList<Character>();
+
+        //generate random vowels
+        for(int i = 0; i < vowelsLength; i++) {
+            letters.add(vowels.charAt(random.nextInt(vowels.length())));
+        }
+
+        // generate random consonants
+        for(int i = 0; i < consonantLength; i++) {
+            letters.add(consonants.charAt(random.nextInt(consonants.length())));
+        }
+
+        Collections.shuffle(letters);
+        return letters;
+    }
+
+    public boolean verifyWord(String word) {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = dictionaryApiUrl + "/" + word;
+        String response = restTemplate.getForObject(url, String.class);
+        return response != null && !response.contains("No Definitions Found") && !response.contains("\"title\"");
+    }
+
+
     public void verifyDuplicate(){}
     public void generatePoints(){}
     public void identifyWinner(){}
--- src/main/java/com/letter_loom/utilities/GameHelper.java
+++
@@ -1,7 +0,0 @@
-package com.letter_loom.utilities;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class GameHelper {
-}
